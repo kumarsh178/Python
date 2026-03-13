52dc1816f52dd88df7ffbebbba50cf1fd0c61355
Shailendra Kumar <kumarsh178@gmail.com>
2026-03-13 16:57:11 +0530

    hospital ai chat bot

--- chatbot/chatbot_ai.py
+++ chatbot/chatbot_ai.py
@@ -1,7 +1,7 @@
 from groq import Groq
 from .rag import search
 
-client = Groq(api_key="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")
+client = Groq(api_key="apikey")
 
 
 def hospital_chat(question):
